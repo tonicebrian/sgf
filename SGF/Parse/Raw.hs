82f73d1218a05b08d2bb8d0a88e074840d00e0b9
Daniel Wagner <dmwit@sorghum.localdomain>
2009-07-30 18:08:37 -0400

    refactoring a bit

---
+++ SGF/Parse/Raw.hs
@@ -0,0 +1,61 @@
+-- boilerplate {{{
+{-# LANGUAGE FlexibleContexts, NoMonomorphismRestriction #-}
+module SGF.Parse.Raw where
+
+import Control.Applicative hiding (many, (<|>))
+import Control.Monad
+import Data.Char
+import Data.Tree
+import Data.Word
+import Prelude hiding (lex)
+import Text.Parsec (SourcePos(..), incSourceColumn)
+import Text.Parsec.Prim
+import Text.Parsec.Combinator
+-- }}}
+data Property = Property SourcePos String [[Word8]]
+
+ensure p x = guard (p x) >> return x
+enum = toEnum . fromEnum
+
+satisfy p = tokenPrim
+    ((\x -> ['\'', x, '\'']) . enum)
+    (\pos _ _ -> incSourceColumn pos 1)
+    (ensure p)
+satisfyChar = satisfy . (. enum)
+
+anyWord     = satisfy (const True)
+exactWord   = satisfy . (==) . enum
+someWord    = satisfy . flip elem . map enum
+noWord      = satisfy . flip notElem . map enum
+
+whitespace  = many (satisfyChar isSpace)
+
+-- assumed: the current byte is literally ASCII '\\' iff the current
+-- byte is the last byte of the encoding of the actual character '\\' and the
+-- byte that is literally ASCII ']' doesn't occur after the first byte of any
+-- multi-byte encoded character
+-- (in particular, UTF-8, ASCII, ISO 8859-1, and EBCDIC satisfy this
+-- property)
+escapedChar             = liftM2 (\x y -> [x, y]) (exactWord '\\') anyWord
+unescapedExcept      ws = fmap return (noWord ws)
+literalTextExcept    ws = fmap concat $ many (escapedChar <|> unescapedExcept ws)
+
+property = liftM3 ((. map enum) . Property)
+    (getPosition)
+    (many1 (satisfyChar (liftM2 (&&) isUpper (< '\128'))))
+    (sepEndBy1 (exactWord '[' >> literalTextExcept "]" <* exactWord ']') whitespace)
+
+node = do
+    exactWord ';'
+    whitespace
+    sepEndBy property whitespace
+
+gameTree = do
+    exactWord '('
+    whitespace
+    (node:nodes) <- sepEndBy1 node     whitespace
+    trees        <- sepEndBy  gameTree whitespace
+    exactWord ')'
+    return (Node node (foldr ((return .) . Node) trees nodes))
+
+collection = whitespace >> many1 gameTree <* whitespace <* eof
