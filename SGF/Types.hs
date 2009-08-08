47ae6d41a4d1eacb45260b9b262ee97a73e86b6b
Daniel Wagner <dmwit@sorghum.localdomain>
2009-08-08 19:17:22 -0400

    formatting only

--- SGF/Types.hs
+++ SGF/Types.hs
@@ -28,28 +28,28 @@ instance Enum GameType where
     toEnum   n = allGameTypesInSGFOrder !! (n - 1)
     fromEnum t = (+1) . fromJust . findIndex (t==) $ allGameTypesInSGFOrder
 
-type Collection       = [Game]
-type Application      = String
-type Version          = String
-type Point            = (Integer, Integer) -- 0-indexed
-type AutoMarkup       = Bool
-data VariationType    = Children  | Siblings deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data Emphasis         = Normal    | Strong   deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data Color            = Black     | White    deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data Certainty        = Uncertain | Certain  deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data InitialPosition  = Beginning | End      deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data RankScale        = Kyu | Dan | Pro      deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data ViewerSetting    = Tried | Marked | LastMove | Headings | Lock             deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data InitialPlacement = Standard | ScrambledEggs | Parachute | Gemma | Custom   deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data RuleSetGo        = AGA | GOE | Chinese | Japanese | NewZealand             deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data RuleSetBackgammon= Crawford | CrawfordGame | Jacoby                        deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data RuleSetOcti      = OctiRuleSet MajorVariation [MinorVariation]             deriving (Eq, Ord, Show, Read)
-data MajorVariation   = Full | Fast | Kids                                      deriving (Eq, Ord, Show, Read, Enum, Bounded)
-data MinorVariation   = Edgeless | Superprong | OtherMinorVariation String      deriving (Eq, Ord, Show, Read)
-data RuleSet a        = Known a | OtherRuleSet String                           deriving (Eq, Ord, Show, Read)
-data WinType          = Score Rational | Resign | Time | Forfeit | OtherWinType deriving (Eq, Ord, Show, Read)
-data GameResult       = Draw | Void | Unknown | Win Color WinType               deriving (Eq, Ord, Show, Read)
-data Rank             = Ranked Integer RankScale (Maybe Certainty) | OtherRank String       deriving (Eq, Ord, Show, Read)
+type Collection         = [Game]
+type Application        = String
+type Version            = String
+type Point              = (Integer, Integer) -- 0-indexed
+type AutoMarkup         = Bool
+data VariationType      = Children  | Siblings  deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data Emphasis           = Normal    | Strong    deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data Color              = Black     | White     deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data Certainty          = Uncertain | Certain   deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data InitialPosition    = Beginning | End       deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data RankScale          = Kyu | Dan | Pro       deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data ViewerSetting      = Tried | Marked | LastMove | Headings | Lock               deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data InitialPlacement   = Standard | ScrambledEggs | Parachute | Gemma | Custom     deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data RuleSetGo          = AGA | GOE | Chinese | Japanese | NewZealand               deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data RuleSetBackgammon  = Crawford | CrawfordGame | Jacoby                          deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data RuleSetOcti        = OctiRuleSet MajorVariation [MinorVariation]               deriving (Eq, Ord, Show, Read)
+data MajorVariation     = Full | Fast | Kids                                        deriving (Eq, Ord, Show, Read, Enum, Bounded)
+data MinorVariation     = Edgeless | Superprong | OtherMinorVariation String        deriving (Eq, Ord, Show, Read)
+data RuleSet a          = Known a | OtherRuleSet String                             deriving (Eq, Ord, Show, Read)
+data WinType            = Score Rational | Resign | Time | Forfeit | OtherWinType   deriving (Eq, Ord, Show, Read)
+data GameResult         = Draw | Void | Unknown | Win Color WinType                 deriving (Eq, Ord, Show, Read)
+data Rank               = Ranked Integer RankScale (Maybe Certainty) | OtherRank String         deriving (Eq, Ord, Show, Read)
 
 data Round = SimpleRound    Integer
            | FormattedRound Integer String
