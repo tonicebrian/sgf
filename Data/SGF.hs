a72c8c0a075bd3d4a263686d8abf51c6a8f18f05
Daniel Wagner <daniel@wagner-home.com>
2010-01-23 23:43:35 -0500

    export Data.Tree

--- Data/SGF.hs
+++ Data/SGF.hs
@@ -9,6 +9,7 @@ module Data.SGF (
     module Data.SGF.Types,
     module Data.SGF.Parse,
     module Data.Word,
+    module Data.Tree,
     module Text.ParserCombinators.Parsec
     -- * Overview of SGF
     -- $sgf
@@ -23,6 +24,7 @@ module Data.SGF (
 import Data.SGF.Types
 import Data.SGF.Parse (collection)
 import Data.Word
+import Data.Tree
 import Text.ParserCombinators.Parsec (runParser)
 
 -- TODO:
